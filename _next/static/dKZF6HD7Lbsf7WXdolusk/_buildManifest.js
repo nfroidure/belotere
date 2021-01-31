b6a8d3f68c9a3f04363ab2643af8e5b7ef65b2f0
Nicolas Froidure <nicolas.froidure@gmail.com>
2021-01-31 10:20:52 +0100

    bec3888

---
+++ _next/static/dKZF6HD7Lbsf7WXdolusk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-4116fc31f59d47624e7f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-acee19d722f65c39929c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/lSjZ-cyMUZnC25GRw7Mpg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-5a44a952df19609bfedf.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-acee19d722f65c39929c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
